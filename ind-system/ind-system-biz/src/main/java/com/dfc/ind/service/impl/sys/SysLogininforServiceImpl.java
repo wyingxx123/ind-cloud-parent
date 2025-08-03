f8122c752b536beed2fd252209fc47fcb852af94
ming <demo@gmail.com>
2025-08-03 11:39:10 +0800

    init system

---
+++ ind-system/ind-system-biz/src/main/java/com/dfc/ind/service/impl/sys/SysLogininforServiceImpl.java
@@ -0,0 +1,67 @@
+package com.dfc.ind.service.impl.sys;
+
+import com.dfc.ind.entity.sys.SysLogininfor;
+import com.dfc.ind.mapper.sys.SysLogininforMapper;
+import com.dfc.ind.service.sys.ISysLogininforService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * 系统访问日志情况信息 服务层处理
+ * 
+ * @author admin
+ */
+@Service
+public class SysLogininforServiceImpl extends ServiceImpl<SysLogininforMapper, SysLogininfor> implements ISysLogininforService
+{
+
+    @Autowired
+    private SysLogininforMapper logininforMapper;
+
+    /**
+     * 新增系统登录日志
+     * 
+     * @param logininfor 访问日志对象
+     */
+    @Override
+    public int insertLogininfor(SysLogininfor logininfor)
+    {
+        return logininforMapper.insertLogininfor(logininfor);
+    }
+
+    /**
+     * 查询系统登录日志集合
+     * 
+     * @param logininfor 访问日志对象
+     * @return 登录记录集合
+     */
+    @Override
+    public List<SysLogininfor> selectLogininforList(SysLogininfor logininfor)
+    {
+        return logininforMapper.selectLogininforList(logininfor);
+    }
+
+    /**
+     * 批量删除系统登录日志
+     * 
+     * @param infoIds 需要删除的登录日志ID
+     * @return
+     */
+    @Override
+    public int deleteLogininforByIds(Long[] infoIds)
+    {
+        return logininforMapper.deleteLogininforByIds(infoIds);
+    }
+
+    /**
+     * 清空系统登录日志
+     */
+    @Override
+    public void cleanLogininfor()
+    {
+        logininforMapper.cleanLogininfor();
+    }
+}
