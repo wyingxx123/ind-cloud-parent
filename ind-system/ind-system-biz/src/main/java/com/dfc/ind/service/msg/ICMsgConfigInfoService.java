bf74b0a3d06076f174fe1006895eb99a8ed0c326
ming <demo@gmail.com>
2025-08-03 11:36:42 +0800

    init system

---
+++ ind-system/ind-system-biz/src/main/java/com/dfc/ind/service/msg/ICMsgConfigInfoService.java
@@ -0,0 +1,16 @@
+package com.dfc.ind.service.msg;
+
+import com.dfc.ind.entity.msg.CMsgConfigInfoEntity;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 消息配置表 服务类
+ * </p>
+ *
+ * @author nwy
+ * @since 2020-04-27
+ */
+public interface ICMsgConfigInfoService extends IService<CMsgConfigInfoEntity> {
+
+}
---
+++ ind-system/ind-system-biz/src/main/java/com/dfc/ind/service/msg/ICMsgMailListService.java
@@ -0,0 +1,16 @@
+package com.dfc.ind.service.msg;
+
+import com.dfc.ind.entity.msg.CMsgMailListEntity;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author nwy
+ * @since 2020-04-27
+ */
+public interface ICMsgMailListService extends IService<CMsgMailListEntity> {
+
+}
