c27db20db5ff5cf6ad9cf0b482f6463fa1eb2454
ming <demo@gmail.com>
2025-08-03 11:37:18 +0800

    init system

---
+++ ind-system/ind-system-biz/src/main/java/com/dfc/ind/service/malluser/IMallUserAddressService.java
@@ -0,0 +1,16 @@
+package com.dfc.ind.service.malluser;
+
+import com.dfc.ind.entity.malluser.MallUserAddressEntity;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 用户地址表 服务类
+ * </p>
+ *
+ * @author ylyan
+ * @since 2020-04-23
+ */
+public interface IMallUserAddressService extends IService<MallUserAddressEntity> {
+
+}
