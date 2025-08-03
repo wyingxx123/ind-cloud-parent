98d00291681e45d5fd3653ab415b5b47da1b904f
ming <demo@gmail.com>
2025-08-03 11:39:20 +0800

    init system

---
+++ ind-system/ind-system-biz/src/main/java/com/dfc/ind/service/impl/sys/SysPermissionServiceImpl.java
@@ -0,0 +1,66 @@
+package com.dfc.ind.service.impl.sys;
+
+
+import com.dfc.ind.entity.sys.SysUser;
+import com.dfc.ind.service.sys.ISysMenuService;
+import com.dfc.ind.service.sys.ISysPermissionService;
+import com.dfc.ind.service.sys.ISysRoleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+public class SysPermissionServiceImpl implements ISysPermissionService
+{
+    @Autowired
+    private ISysRoleService roleService;
+
+    @Autowired
+    private ISysMenuService menuService;
+
+    /**
+     * 获取角色数据权限
+     *
+     * @param userId 用户Id
+     * @return 角色权限信息
+     */
+    @Override
+    public Set<String> getRolePermission(Long userId)
+    {
+        Set<String> roles = new HashSet<String>();
+        // 管理员拥有所有权限
+        if (SysUser.isAdmin(userId))
+        {
+            roles.add("admin");
+        }
+        else
+        {
+            roles.addAll(roleService.selectRolePermissionByUserId(userId));
+        }
+        return roles;
+    }
+
+    /**
+     * 获取菜单数据权限
+     *
+     * @param userId 用户Id
+     * @return 菜单权限信息
+     */
+    @Override
+    public Set<String> getMenuPermission(Long userId,Long merchantId)
+    {
+        Set<String> perms = new HashSet<String>();
+        // 管理员拥有所有权限
+        if (SysUser.isAdmin(userId))
+        {
+            perms.add("*:*:*");
+        }
+        else
+        {
+            perms.addAll(menuService.selectMenuPermsByUserId(userId,merchantId));
+        }
+        return perms;
+    }
+}
